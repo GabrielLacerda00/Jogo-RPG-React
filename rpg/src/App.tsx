4278ee107f93b7b390c9333d381ac6d8142ea587
Gabriel Lacerda <gabriel.lacerd.gr@gmail.com>
2022-01-19 08:50:05 -0300

    commit all

---
+++ rpg/src/App.tsx
@@ -0,0 +1,47 @@
+import {useEffect} from 'react';
+import * as C from './App.styles';
+import {Character} from './components/Character';
+import {useCharacter} from './hooks/userCharacter';
+
+const App = () => {
+
+  const char = useCharacter('Gabriel');
+
+  useEffect(() =>{
+      window.addEventListener('keydown',handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+          char.moveLeft();
+      break;
+
+      case 'KeyW':
+      case 'ArrowUp':
+          char.moveUp();
+      break;
+
+      case 'keyD':
+      case 'ArrowRight':
+         char.moveRight();
+      break;
+
+      case 'KeyS':
+      case 'ArrowDown':
+          char.moveDown();
+      break;
+    }
+  }
+
+  return (
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side}/>
+      </C.Map>
+    </C.Container>
+  );
+}
+
+export default App;
